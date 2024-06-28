d158812036958622bc08ee6b52c2b5acf333dbe9
Amit kumar jha <159288053+Amit-jha98@users.noreply.github.com>
2024-06-28 17:07:01 +0530

    cal si final.c

--- cal si final.c
+++ cal si final.c
@@ -2,36 +2,28 @@
 #include <math.h>
 
 int main()
- {
+{
     double m1, m2, vf, k, mu;
+    double wavelength, wave_number;
+    double speed_of_light = 3.00e+8;
 
-double wavelength, wave_number;
-
-double speed_of_light = 3.00e+8;
-    // Input mass of atom 1 in atomic mass units (amu)
     printf("Enter the mass of atom 1 (in kg): ");
     scanf("%lf", &m1);
 
-    // Input mass of atom 2 in atomic mass units (amu)
     printf("Enter the mass of atom 2 (in kg): ");
     scanf("%lf", &m2);
 
-    // Input vibrational frequency in cm^-1
     printf("Enter the vibrational frequency (in Hz) or enter 0 (if you want to find): ");
     scanf("%lf", &vf);
 
-    // Input force constant (k) in dyn/cm or enter 0 (if you want to find)
     printf("Enter the value of Force Constant (K) (in N/m) or enter 0 (if you want to find): ");
     scanf("%lf", &k);
 
-    // Convert mass to kg
-    m1 *= 1.66054e-27;  // 1 amu = 1.66054e-27 kg
+    m1 *= 1.66054e-27;  
     m2 *= 1.66054e-27;
 
-    // Convert force constant to N/m
-    k *= 1.0e4;  // 1 dyn/cm = 1.0e-4 N/m
+    k *= 1.0e4;
 
-    // Calculate reduced mass in kg
     mu = (m1 * m2) / (m1 + m2);
 
     if (k == 0 && vf == 0) {
@@ -45,8 +37,8 @@ double speed_of_light = 3.00e+8;
         printf("Vibrational Frequency (v): %.4e Hz\n", vf);
     } else {
         vf = (1.0 / (2 * M_PI)) * sqrt(k / mu);
- wavelength = speed_of_light / vf;
-wave_number = 1.0 / wavelength;
+        wavelength = speed_of_light / vf;
+        wave_number = 1.0 / wavelength;
 
         printf("\nResults:\n");
         printf("\nReduced Mass (μ): %.4e kg\n", mu);
