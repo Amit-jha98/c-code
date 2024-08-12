75447a1f93a977c35d2d49f9ebdecff1609abd7d
Amit kumar jha <sumitkr95078@gmail.com>
2024-08-12 19:54:49 +0530

    Update loop prec.c

--- loop prec.c
+++ loop prec.c
@@ -7,15 +7,12 @@ int main() {
     scanf("%d", &i);
 
     for (int a = 1; a <= i; a++) {
-        // Print spaces before numbers
         for (int s = i - a; s > 0; s--) {
             printf(" ");
         }
-        // Print numbers in increasing order
         for (j = 1; j <= a; j++) {
             printf("%d", j);
         }
-        // Print numbers in decreasing order
         for (j = a - 1; j > 0; j--) {
             printf("%d", j);
         }
